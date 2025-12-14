5c0fae11f96d3ced948fc0e8092ee688c4f2c0f9
Muhammed Rinshan <88284416+NIGHTWALKER3@users.noreply.github.com>
2025-12-14 17:36:54 +0530

    Add files via upload

---
+++ lib/services/music_provider.dart
@@ -0,0 +1,14 @@
+import 'api_interface.dart';
+import 'jamendo_service.dart';
+
+class MusicProvider {
+  static final MusicAPI _api = JamendoService();
+
+  static Future<List<Map<String, dynamic>>> getByGenre(String genre) {
+    return _api.fetchByGenre(genre);
+  }
+
+  static Future<List<Map<String, dynamic>>> search(String query) {
+    return _api.searchMusic(query);
+  }
+}
